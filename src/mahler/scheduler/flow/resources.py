b14b3760a6871dc6202ecf0b95115dc4ddbf3e3c
Xavier Bouthillier <xavier.bouthillier@umontreal.ca>
2018-12-29 11:53:39 -0500

    Creation with cookiecutter

---
+++ src/mahler/scheduler/flow/resources.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+"""
+:mod:`mahler.scheduler.flow.resources -- TODO
+===================================
+
+.. module:: resources
+    :platform: Unix
+    :synopsis: TODO
+
+TODO: Write long description
+
+"""
+from mahler.core.resources import Resources
+
+
+class FlowResources(Resources):
+    """
+    """
+
+    def __init__(self):
+        """
+        """
+        pass
+
+    def available(self):
+        """
+        """
+        raise NotImplemented()
+
+    def submit(self, tasks, **kwargs):
+        """
+        """
+        pass
