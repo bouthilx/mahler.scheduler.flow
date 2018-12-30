8ce900f957d999daac7535822b1ad7d9da2d7425
Xavier Bouthillier <xavier.bouthillier@umontreal.ca>
2018-12-30 08:44:17 -0500

    Implement FlowResources

--- src/mahler/scheduler/flow/resources.py
+++ src/mahler/scheduler/flow/resources.py
@@ -10,24 +10,79 @@
 TODO: Write long description
 
 """
+import getpass
+import os
+import subprocess
+
 from mahler.core.resources import Resources
+
+SUBMISSION_ROOT = os.environ['FLOW_SUBMISSION_DIR']
+
+FLOW_OPTIONS_TEMPLATE = "{array}mem=20000M;time=2:59:00;job-name={job_name}"
+
+FLOW_TEMPLATE = "flow-submit {container} --config {file_path} --options {options}"
+
+COMMAND_TEMPLATE = "mahler execute --container {container} --tags {tags}"
+
+SUBMIT_COMMANDLINE_TEMPLATE = "{flow} launch {command}"
 
 
 class FlowResources(Resources):
     """
     """
 
-    def __init__(self):
+    def __init__(self, max_workers=100):
         """
         """
-        pass
+        self.max_workers = max_workers
 
     def available(self):
         """
         """
-        raise NotImplemented()
+        command = 'squeue -r -o %t -u {user}'.format(user=getpass.getuser())
+        out = subprocess.check_output(command.split(" "))
+        out = str(out, encoding='utf-8')
+        states = dict()
+        for line in out.split("\n")[1:]:  # ignore `ST` header
+            if line not in states:
+                states[line] = 0
 
-    def submit(self, tasks, **kwargs):
+            states[line] += 1
+
+        total_jobs = sum(number for name, number in states.items() if name != 'CG')
+        return max(self.max_workers - total_jobs, 0)
+
+    def submit(self, tasks, container, tags=tuple()):
         """
         """
-        pass
+        nodes_available = self.available()
+        if not nodes_available:
+            return
+        array_option = 'array=1-{};'.format(min(len(tasks), nodes_available))
+        flow_options = FLOW_OPTIONS_TEMPLATE.format(
+            array=array_option, job_name=".".join(sorted(tags)))
+
+        submission_dir = os.path.join(SUBMISSION_ROOT, container)
+        if not os.path.isdir(submission_dir):
+            os.makedirs(submission_dir)
+
+        if tags:
+            file_name = ".".join(tag for tag in sorted(tags) if tag) + ".sh"
+        else:
+            file_name = "all.sh"
+
+        file_path = os.path.join(submission_dir, file_name)
+
+        flow_command = FLOW_TEMPLATE.format(
+            container=container, file_path=file_path, options=flow_options)
+
+        command = COMMAND_TEMPLATE.format(container=container, tags=" ".join(tags))
+
+        submit_command = SUBMIT_COMMANDLINE_TEMPLATE.format(flow=flow_command, command=command)
+
+        print("Executing:")
+        print(submit_command)
+        out = subprocess.check_output(submit_command.split(" "))
+        print("\nCommand output")
+        print("------")
+        print(str(out, encoding='utf-8'))
