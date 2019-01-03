3a9b4be63585c561bb0deab86b63c2975b971b5c
Xavier Bouthillier <xavier.bouthillier@umontreal.ca>
2019-01-03 13:48:37 -0500

    Add support for config

--- src/mahler/scheduler/flow/__init__.py
+++ src/mahler/scheduler/flow/__init__.py
@@ -9,6 +9,11 @@
 
 TODO: Write long description
 """
+import os
+
+import mahler.core
+import mahler.core.utils.config
+
 from ._version import get_versions
 from .resources import FlowResources
 
@@ -24,6 +29,12 @@ __author_email__ = 'xavier.bouthillier@umontreal.ca'
 __copyright__ = u'2018, Xavier Bouthillier'
 __url__ = 'https://github.com/bouthilx/mahler.scheduler.flow'
 
+DEF_CONFIG_FILES_PATHS = [
+    os.path.join(mahler.core.DIRS.site_data_dir, 'scheduler', 'flow', 'config.yaml.example'),
+    os.path.join(mahler.core.DIRS.site_config_dir, 'scheduler', 'flow', 'config.yaml'),
+    os.path.join(mahler.core.DIRS.user_config_dir, 'scheduler', 'flow', 'config.yaml')
+    ]
+
 
 def build(max_workers, **kwargs):
     return FlowResources(max_workers=max_workers)
@@ -36,3 +47,20 @@ def build_parser(parser):
     flow_parser.add_argument(
         '--max-workers', type=int,
         help='number of concurrent workers to submit.')
+
+
+def define_config():
+    config = mahler.core.utils.config.Configuration()
+    config.add_option(
+        'max_workers', type=int, default=1, env_var='MAHLER_SCHEDULER_FLOW_MAX_WORKERS')
+
+    return config
+
+
+def parse_config_files(config):
+    mahler.core.utils.config.parse_config_files(
+        config, mahler.core.DEF_CONFIG_FILES_PATHS,
+        base='scheduler.flow')
+
+    mahler.core.utils.config.parse_config_files(
+        config, DEF_CONFIG_FILES_PATHS)
